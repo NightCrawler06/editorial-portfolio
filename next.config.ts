21b3c994fce10f1c17a728278b7345130cf25776
NightCrawler06 <euelv720@gmail.com>
2026-05-24 14:34:58 +0800

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ import { fileURLToPath } from "node:url";
 const root = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  basePath: "/new-portfolio",
-  assetPrefix: "/new-portfolio",
   turbopack: {
     root,
   },
